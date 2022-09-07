f545651e1498ea057a546577813b9eda9f56a7bf
yuexiaosheng <gehuajun@moxiu.net>
2022-09-07 10:16:54 +0800

    add project nacos1

---
+++ nacos1/src/main/java/com/javaer/nacos1/controller/Config.java
@@ -0,0 +1,22 @@
+package com.javaer.nacos1.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("config")
+public class Config {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
+
+}
---
+++ nacos1/src/test/java/com/javaer/nacos1/Nacos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaer.nacos1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nacos1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
